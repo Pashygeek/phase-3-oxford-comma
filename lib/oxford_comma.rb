4360f3928075bacfad12f0a79038cc1f8a9ff282
patiencewanjiru <patiencewanjiru123@gmail.com>
2023-05-21 23:35:41 +0300

    Completed assignment

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  case array.length
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  else
+    array[0..-2].join(", ") + ", and " + array[-1]
+  end
 end
+
+array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+string = oxford_comma(array)
+puts string
